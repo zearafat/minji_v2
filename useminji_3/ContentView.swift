a2be9fb80f1f9da56ab76573d79c65202ae37c27
zeaarafat <alzea.arafat@icloud.com>
2024-04-30 09:19:21 +0700

    Initial Commit

---
+++ useminji_3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  useminji_3
+//
+//  Created by Alzea Arafat on 30/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ useminji_3/useminji_3App.swift
@@ -0,0 +1,17 @@
+//
+//  useminji_3App.swift
+//  useminji_3
+//
+//  Created by Alzea Arafat on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct useminji_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
